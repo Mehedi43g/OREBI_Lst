c25ff094191c45912462cb8c2dbc54f4a9807bc8
Mehedi Hasan <xdemyyt@gmail.com>
2025-09-10 21:59:33 +0600

    RTX

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,15 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+  ],
+  plugins: [require("flowbite/plugin")],
+}
 
 export default defineConfig([
   globalIgnores(['dist']),
--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "node_modules/flowbite-react/lib/esm/**/*.js",
-  ],
-  plugins: [require("flowbite/plugin")],
-};
-
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
 })
-
