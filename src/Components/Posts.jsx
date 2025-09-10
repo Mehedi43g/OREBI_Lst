3a2be24b7e7d342711bd05953483ea2cfc8b1679
Mehedi Hasan <xdemyyt@gmail.com>
2025-09-11 03:50:42 +0600

    fixd

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -57,7 +57,9 @@ const Posts = ({ allPage, filterCategory, active }) => {
   return (
     <div className="">
       {allFilter.length > 0 ? (
+        <>
         <div className="">
+          {active ? <div className="">
           <div className="grid md:grid-cols-3 gap-4">
             {allFilter.map((item) => (
               <div className="h-[400px]" key={item.id}>
@@ -126,7 +128,88 @@ const Posts = ({ allPage, filterCategory, active }) => {
               </div>
             )}
           </div>
+        </div>: 
+        <div className="">
+          <div className="">
+            {allFilter.map((item) => (
+              <div className="h-[300px]" key={item.id}>
+                    <div className="relative h-full overflow-hidden shadow-md group flex items-center">
+                      <Link to={`/Shop/${item.id}`}>
+                        <img
+                          className="w-[80%] cursor-pointer object-cover "
+                          src={item.thumbnail}
+                          alt=""
+                        />
+                      </Link>
+                      <div className="absolute top-4 left-3 z-5">
+                        <p className="py-[5px] px-[20px] border bg-black text-white font-bold text-[14px] font-dm">
+                          {item.discountPercentage}%
+                        </p>
+                      </div>
+                      <div className="absolute right-0 bottom-0 py-4">
+                        <div className="flex gap-4">
+
+                          <div className="w-[170px] flex items-center justify-end gap-2 py-3 px-4 rounded-2xl shadow-md bg-white/80 backdrop-blur-sm border border-gray-200 hover:bg-pink-50 hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                            <p className="text-sm font-medium text-gray-700 group-hover:text-pink-600">Add to Wish List</p>
+                            <GiSelfLove className="text-gray-500 text-lg group-hover:text-pink-600 transition-colors duration-300" />
+                          </div>
+
+
+                          <div className="w-[170px] flex items-center justify-end gap-2 py-3 px-4 rounded-2xl shadow-md bg-white/80 backdrop-blur-sm border border-gray-200 hover:bg-blue-50 hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                            <TfiReload className="text-gray-500 text-lg group-hover:text-blue-600 transition-colors duration-300" />
+                            <p className="text-sm font-medium text-gray-700 group-hover:text-blue-600">Reload</p>
+                          </div>
+
+
+                          <div className="w-[170px] flex items-center justify-end gap-2 py-3 px-4 rounded-2xl shadow-md bg-white/80 backdrop-blur-sm border border-gray-200 hover:bg-green-50 hover:shadow-lg transition-all duration-300 cursor-pointer group">
+                            <p className="text-sm font-medium text-gray-700 group-hover:text-green-600">Add to Cart</p>
+                            <FaShoppingCart className="text-gray-500 text-lg group-hover:text-green-600 transition-colors duration-300" />
+                          </div>
+                        </div>
+                      </div>
+
+                      <div className=" justify-between pl-[20px] pr-[20px]  ">
+                        <div>
+                          <p className="py-[10px] font-bold font-dm text-[16px] text-[#262626]">
+                            {item.title}
+                          </p>
+                          <p className="py-[10px] font-bold font-dm text-[16px] text-[#8a8686]">
+                            {item.category}
+                          </p>
+                        </div>
+                        <div>
+                          <p className="py-[10px] font-dm text-[#767676]">${item.price} </p>
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+            ))}
+          </div>
+
+          <div className="pt-[10px]">
+            {showAll ? (
+              filterCategory.length > 5 && (
+                <div
+                  onClick={handleShow}
+                  className="w-[120px] text-center flex items-center gap-2 px-6 py-3 text-white font-semibold text-base rounded-lg bg-gradient-to-r from-[#000000] to-[#043a9770] overflow-hidden transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:translate-y-[-2px] hover:rounded-[20px] hover:shadow-[0_5px_15px_rgba(106,17,203,0.3)] dark:from-[#180f25] dark:to-[#3a7bd5] dark:hover:shadow-[0_5px_15px_rgba(138,62,255,0.3)] cursor-pointer"
+                >
+                  <button>Show All</button>
+                </div>
+              )
+            ) : (
+              <div
+                onClick={handleLess}
+                className="w-[130px] text-center flex items-center gap-2 px-6 py-3 text-white font-semibold text-base rounded-lg bg-gradient-to-r from-[#000000] to-[#043a9770] overflow-hidden transition-all duration-300 ease-[cubic-bezier(0.4,0,0.2,1)] hover:translate-y-[-2px] hover:rounded-[20px] hover:shadow-[0_5px_15px_rgba(106,17,203,0.3)] dark:from-[#180f25] dark:to-[#3a7bd5] dark:hover:shadow-[0_5px_15px_rgba(138,62,255,0.3)] cursor-pointer"
+              >
+                <button>Show Less</button>
+              </div>
+            )}
+          </div>
         </div>
+           }
+        </div>
+        </>
       ) : (
         <div className=''>
           {active ?
