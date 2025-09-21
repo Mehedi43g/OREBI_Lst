dea1d86ff14a356ad9c647ef2d82b103999df8a5
Mehedi Hasan <xdemyyt@gmail.com>
2025-09-22 04:22:02 +0600

    cat

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../Components/Container'
 
 const Contacts = () => {
+  let [text, settext] = useState('')
+  let handleInput = (e)=>{
+    settext(e.target.value);
+    
+  }
+
+  let handleSubmit = ()=>{
+    alert(text)
+  }
   return (
     <div>
       <Container>
         <div className="">
           <div className="">
-            <input className='border-1 p-[10px]' type="text" placeholder='orange'/>
-            <button className='ml-[10px] p-[10px] cursor-pointer border-1'>Submite</button>
+            <input onChange={handleInput} className='border-1 p-[10px]' type="text" placeholder='orange'/>
+            <button onClick={handleSubmit} className='ml-[10px] p-[10px] cursor-pointer border-1'>Submite</button>
           </div>
       <h3>Contacts</h3>
         </div>
